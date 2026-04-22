4de7de81a4ad76c2e3b8ed24ed4afa67704a3c1a
HansBans_ <hm.baks@gmail.com>
2026-04-22 14:14:47 +0200

    Showcasing scrollbar

--- src/components/MyProjects.ts
+++ src/components/MyProjects.ts
@@ -8,4 +8,6 @@ export interface MyProjects {
 export const projects: MyProjects[] = [
     {id: 1, name: "My Github Page", tags: ["React", "TS", "Vite"], description: "My page on Github"},
     {id: 2, name: "To be done", tags: ["TBD"], description: "Future project"},
+    {id: 3, name: "To be done", tags: ["TBD"], description: "Future project"},
+    {id: 4, name: "To be done", tags: ["TBD"], description: "Future project"},
 ];
