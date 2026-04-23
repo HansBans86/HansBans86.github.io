d84b57e5277f5221f928bdb69763ff40718a3879
HansBans_ <hm.baks@gmail.com>
2026-04-23 23:18:02 +0200

    Added URLs to Project datatype

--- src/components/MyProjects.ts
+++ src/components/MyProjects.ts
@@ -3,11 +3,28 @@ export interface MyProjects {
     name: string;
     tags: string[];
     description: string;
+    link: string;
 }
 
 export const projects: MyProjects[] = [
-    {id: 1, name: "My Github Page", tags: ["React", "TS", "Vite"], description: "My page on Github"},
-    {id: 2, name: "To be done", tags: ["TBD"], description: "Future project"},
-    {id: 3, name: "To be done", tags: ["TBD"], description: "Future project"},
-    {id: 4, name: "To be done", tags: ["TBD"], description: "Future project"},
+    {
+        id: 1, name: "My Github Page", tags: ["React", "TypeScript", "Vite"],
+        description: "My page on Github",
+        link: "https://github.com/HansBans86/HansBans86.github.io"
+    },
+    {
+        id: 2, name: "To be done", tags: ["TBD"],
+        description: "Future project",
+        link: ""
+    },
+    {
+        id: 3, name: "To be done", tags: ["TBD"],
+        description: "Future project",
+        link: ""
+    },
+    {
+        id: 4, name: "To be done", tags: ["TBD"],
+        description: "Future project",
+        link: ""
+    },
 ];
